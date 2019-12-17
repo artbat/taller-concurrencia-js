a4730ca09ac7eb8051074a9ce32a50023113ac83
Arturo Batanero <abatanero@paradigmadigital.com>
2019-12-17 11:36:02 +0100

    Creamos la clase Photo que representa en el DOM los datos descargados y utilizamos el catch para representar el error de descarga

--- js/app.js
+++ js/app.js
@@ -1,11 +1,57 @@
 
 const photosURL = 'https://picsum.photos/v2/list';  // 'js/photos.json';
+const main = document.getElementById('main');
 
+/**
+ * La clase Photo guarda la fracción del DOM y los datos que representan a cada foto que nos descargamos
+ *
+ * Schema -> {
+ *  "id": "0",
+ *  "author": "Alejandro Escamilla",
+ *  "width": 5616,
+ *  "height": 3744,
+ *  "download_url": "https://picsum.photos/id/0/5616/3744"
+ */
+class Photo {
+   // Crea el DOM para representar la foto
+   constructor() {
+      this.div = document.createElement('div');
+      this.img = document.createElement('img');
+      this.span = document.createElement('span');
+
+      this.div.appendChild(this.img);
+      this.div.appendChild(this.span);
+   }
+
+   // Guarda y rellena los datos en la foto, añadiéndola al DOM
+   append(data) {
+      this.data = data;
+      this.div.id = 'photo-' + data.id;
+      this.span.innerHTML = `${data.author}`;
+      this.img.src = data.download_url;
+      main.appendChild(this.div);
+   }
+}
+
+// Obtenemos los datos de las fotos y las convertimos a JSON
 window.fetch(photosURL)
    .then(res => res.json())
+
+   // Creamos los objetos Photo y los añadimos al DOM
    .then(photos => {
-      console.log(`tenemos ${photos.length} fotos!`);
+      photos.map(photoData => {
+         const photo = new Photo();
+         photo.append(photoData);
+      });
    })
+
+   // Si se produce algún error de ejecución cargamos un placeholder de error
+   // Estos errores pueden ser: la descarga del fetch, el parseado del json o un error JS en alguna función
    .catch(error => {
+      const photo = new Photo();
+      photo.append({
+         'download_url': 'img/no-network.png',
+         'author': 'No hay datos por no haber conexión de red!'
+      });
       console.log('No tenemos datos de fotos!');
    });
